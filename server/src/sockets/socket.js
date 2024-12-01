5a1c370f96bff6ca4f289f920924dc2d0d14d2ef
JosefinSans <filcinasona@gmail.com>
2024-12-01 18:07:48 +0300

    cors

--- server/src/sockets/socket.js
+++ server/src/sockets/socket.js
@@ -7,7 +7,7 @@ import cors from "cors";
 export const socketHandler = (server) => {
   const io = new Server(server, {
     cors: {
-      origin: "http://localhost:5174",
+      origin: "*",
     },
   });
 
